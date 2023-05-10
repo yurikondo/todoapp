5c0dc37ea909f5000903d4070e6e679d0417c3e0
yuri <yuri@example.com>
2023-05-11 08:26:01 +0900

    Play 25. APIのエンドポイントを指定してHTTPメソッドを追加してみよう

--- app.js
+++ app.js
@@ -2,4 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+app.get("/api/v1/tasks", (req, res) => {
+  res.send("タスク全取得");
+});
+
+app.post("/api/v1/tasks", (req, res) => {
+  res.send("タスクを新規作成");
+});
+
+app.get("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスク");
+});
+
+app.patch("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスクを更新");
+});
+
+app.delete("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスクを削除");
+});
+
+
+
 app.listen(PORT, console.log("サーバー起動中🚀"));
