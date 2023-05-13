9b5bad8bdbd6b7e60d28dd7498c241cf2c48ca0f
yuri <yuri@example.com>
2023-05-13 20:13:06 +0900

    56. 完了・未完了状態を編集してみよう

--- public/editTask.js
+++ public/editTask.js
@@ -1,6 +1,9 @@
 const taskIdDOM = document.querySelector(".task-edit-id");
 const taskNameDOM = document.querySelector(".task-edit-name");
- 
+const editFormDOM = document.querySelector(".single-task-form");
+const formAlertDOM = document.querySelector(".form-alert");
+const taskECompletedDOM = document.querySelector(".task-edit-completed");
+
 const params = window.location.search;
 const id = new URLSearchParams(params).get("id");
 
@@ -17,3 +20,26 @@ const showTask = async () => {
   }
 };
 showTask();
+
+//タスクの編集
+editFormDOM.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const taskName = taskNameDOM.value;
+    taskCompleted = taskECompletedDOM.checked;
+    const { data: task } = await axios.patch(`/api/v1/tasks/${id}`, {
+      name: taskName,
+      completed: true,
+    });
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = "編集に成功しました";
+    formAlertDOM.classList.add("text-success");
+  } catch (err) {
+    console.log(err);
+  }
+
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+    formAlertDOM.classList.remove("text-success");
+  }, 3000);
+});
