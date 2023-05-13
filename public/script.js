8d4e4885b6831272dcffca6ecc7124dd4adbad15
yuri <yuri@example.com>
2023-05-13 09:16:51 +0900

    45. axiosを使ってNode.jsサーバーと連携してタスク全て取得しよう

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
-console.log("アキシオス");
+// /api/v1/tasks からタスクを読み込む
+
+const showTasks = async () => {
+  try {
+    // 自作のAPIをたたく
+    // dataという属性をtasksとして取得　
+    const { data: tasks } = await axios.get("/api/v1/tasks");
+    console.log(tasks);
+  } catch (err) {
+    console.log(err);
+  }
+};
+showTasks();
