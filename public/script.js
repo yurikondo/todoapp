ebcee5b1d17e66ad97a3dc1efb9805aa781614cb
yuri <yuri@example.com>
2023-05-13 16:40:12 +0900

    50. データ作成のエラーハンドリングをしてみよう

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const taskDOM = document.querySelector(".tasks");
 const formDOM = document.querySelector(".task-form");
 const taskInputDOM = document.querySelector(".task-input");
+const formAlertDOM = document.querySelector(".form-alert");
 
 // /api/v1/tasks からタスクを読み込む
 const showTasks = async () => {
@@ -8,6 +9,12 @@ const showTasks = async () => {
     // 自作のAPIをたたく
     // dataという属性をtasksとして取得
     const { data: tasks } = await axios.get("/api/v1/tasks");
+
+    //タスクが一つもない時
+    if (tasks.length < 1) {
+      taskDOM.innerHTML = `<H5 class="empty-list">タスクがありません</H5>`;
+      return;
+    }
 
     //タスクを出力
     const allTasks = tasks
@@ -47,9 +54,18 @@ formDOM.addEventListener("submit", async (event) => {
     await axios.post("/api/v1/tasks", { name: name });
     showTasks();
     taskInputDOM.value = "";
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = "タスクを追加しました";
+    formAlertDOM.classList.add("text-success");
   } catch (err) {
     console.log(err);
+    formAlertDOM.style.display = "block";
+    formAlertDOM.innerHTML = "無効です。もう一度やり直してください";
   }
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+    formAlertDOM.classList.remove("text-success");
+  }, 3000);
 });
 
 //タスクを削除する
