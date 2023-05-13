5818938429321f7538b5c7cb230ddcb7058002bf
yuri <yuri@example.com>
2023-05-13 19:36:01 +0900

    54. タスクIDを使って特定のタスクを１つだけ取得して表示しよう

--- public/editTask.js
+++ public/editTask.js
@@ -1,3 +1,19 @@
+const taskIdDOM = document.querySelector(".task-edit-id");
+const taskNameDOM = document.querySelector(".task-edit-name");
+ 
 const params = window.location.search;
 const id = new URLSearchParams(params).get("id");
-console.log(id);
+
+//一つの特定のタスクを表示する
+const showTask = async () => {
+  try {
+    // dataという属性をtaskとして取得
+    const { data: task } = await axios.get(`/api/v1/tasks/${id}`);
+    const { _id, completed, name } = task;
+    taskIdDOM.textContent = _id;
+    taskNameDOM.value = name;
+  } catch (err) {
+    console.log(err);
+  }
+};
+showTask();
